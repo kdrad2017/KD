0809a0516be2ee7a2f8de29e2f0b7887742ce4d2
qaz82975 <>
2022-07-12 08:17:45 +0800

    7/12

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+
+
+var i = 1;
+function nav_rwd() {
+    i++;
+    var nav_none = document.getElementById('nav_none');
+    if (i % 2 == 0) { nav_none.style.display = 'block';}
+    else { nav_none.style.display = 'none'; }
+
+   
+}
+
+ async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"
---
+++ JS/vue.js
@@ -0,0 +1,41 @@
+// let data={
+//     menu:['別讓高血壓悄悄近身！ e世代吃重鹹,高血壓自知率不到3成！','03'],
+   
+    
+//   }
+  
+//   new Vue({
+//     el:'#app',
+//     data:data,
+    
+   
+    
+//   })
+// {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
+let data ={
+   
+  menus:[
+    {date:'2022/7/7',title:'標題一',main:'內容1'},
+    {date:'2022/7/6',title:'標題二',main:'內容2'},
+    {date:'2022/6/6',title:'標題三',main:'內容3'}
+  ],
+ 
+}
+
+let vm=new Vue({
+  el:'#app',
+  data:data,
+
+})
+
+
+    // new Vue({
+    //     el:'#app',
+    //     data:{
+    //         items: [
+    //           { message: 'Apple' },
+    //           { message: 'banana' },
+    //           { message: 'Coconut' }
+    //         ]
+    //     }
+    // });
